d360d501283d6765a8c5209cc7a804cebcb8c695
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-18 12:30:25 +0100

    Add files via upload

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import $ from 'jquery';
+
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+  let count = 0;
+
+  const updateCounter = () => {
+    count++;
+    $('#count').html(`${count} clicks on the button`);
+  }
+  $("button").on("click", _.debounce(updateCounter, 1000));
+  
