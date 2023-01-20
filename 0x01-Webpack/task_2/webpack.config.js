f3bbc71ae2f6abaf43e68dfd04f0e130d7dad202
Lamine Mokhtar Jemli <113144795+laminemokhtarjemli@users.noreply.github.com>
2023-01-20 12:45:56 +0100

    Add files via upload

---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpg$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            limit: 200000,
+          }
+        }
+      }
+    ]
+  }
+};
